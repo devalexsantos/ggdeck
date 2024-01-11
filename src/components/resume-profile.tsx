8458c17257793003e138cd07322ae2a576334516
Alex Santos <devalexsantos@gmail.com>
2024-01-11 12:16:02 -0300

    aditioned header bar and change to dark default colors globals css

--- src/components/resume-profile.tsx
+++ src/components/resume-profile.tsx
@@ -1,6 +1,6 @@
 export default function ResumeProfile() {
   return (
-    <div className="flex flex-col gap-4 text-zinc-300 w-full">
+    <div className="flex flex-col gap-4 w-full">
       <div className="flex justify-between px-5 py-2 bg-zinc-950 rounded-xl">
         <span>Jogos zerados:</span>
         <span>32</span>
@@ -9,7 +9,7 @@ export default function ResumeProfile() {
       <div className="flex flex-col gap-4 px-5 py-2 bg-zinc-950 rounded-xl">
         <span>Jogando atualmente:</span>
         <div className="flex flex-col gap-2">
-          <span className="text-zinc-400">Final Fantasy XVI</span>
+          <span className="text-muted-foreground">Final Fantasy XVI</span>
         </div>
       </div>
     </div>
--- src/pages/_layout/app.tsx
+++ src/pages/_layout/app.tsx
@@ -4,19 +4,27 @@ import ResumeProfile from '@/components/resume-profile'
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen flex flex-col sm:flex-row">
-      <aside className="bg-zinc-900 w-full p-8 flex flex-col items-center gap-4 sm:w-[300px]">
-        <div className="flex flex-col gap-6">
-          <Avatar className="w-28 h-28">
-            <AvatarImage src="https://github.com/devalexsantos.png" />
-            <AvatarFallback>AS</AvatarFallback>
-          </Avatar>
-          <span className="text-xl font-bold text-zinc-300">Alex Santos</span>
+    <>
+      <div className="w-full flex p-3 bg-zinc-950 border-b border-b-zinc-500">
+        <div className="w-full max-w-[300px] flex justify-start sm:justify-center">
+          <span className="text-2xl italic font-extrabold">ggdeck.</span>
         </div>
+        <div className="flex-1 flex justify-end"></div>
+      </div>
+      <div className="min-h-screen flex flex-col sm:flex-row">
+        <aside className="bg-zinc-900 w-full p-8 flex flex-col items-center gap-4 sm:w-[300px]">
+          <div className="flex flex-col gap-6">
+            <Avatar className="w-28 h-28">
+              <AvatarImage src="https://github.com/devalexsantos.png" />
+              <AvatarFallback>AS</AvatarFallback>
+            </Avatar>
+            <span className="text-xl font-bold ">Alex Santos</span>
+          </div>
 
-        <ResumeProfile />
-      </aside>
-      <Outlet />
-    </div>
+          <ResumeProfile />
+        </aside>
+        <Outlet />
+      </div>
+    </>
   )
 }
