bafa1522199e9fe73411c2cbbe2bc625fc83c524
Alex Santos <devalexsantos@gmail.com>
2024-01-13 10:46:40 -0300

    chore: editing resume layout

--- src/components/resume-profile.tsx
+++ src/components/resume-profile.tsx
@@ -1,16 +1,9 @@
 export default function ResumeProfile() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-2">
       <div className="flex justify-between px-5 py-2 bg-zinc-950 rounded-xl">
         <span>Jogos zerados:</span>
         <span>32</span>
-      </div>
-
-      <div className="flex flex-col gap-4 px-5 py-2 bg-zinc-950 rounded-xl">
-        <span>Jogando atualmente:</span>
-        <div className="flex flex-col gap-2">
-          <span className="text-muted-foreground">Final Fantasy XVI</span>
-        </div>
       </div>
     </div>
   )
