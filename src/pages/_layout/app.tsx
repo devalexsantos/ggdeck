76d63d751c704cd604b08e7746ac1ecfe3f09d39
Alex Santos <devalexsantos@gmail.com>
2024-01-13 10:47:08 -0300

    chore: modify header layout

--- src/pages/_layout/app.tsx
+++ src/pages/_layout/app.tsx
@@ -10,7 +10,7 @@ export default function AppLayout() {
   return (
     <>
       <div className="w-full flex p-3 bg-zinc-950 border-b border-b-zinc-500 border-opacity-50">
-        <div className="flex-1 flex w-full px-8 justify-end items-center sm:justify-between">
+        <div className="flex-1 flex w-full px-8 items-center justify-between">
           <span className="text-2xl italic font-extrabold">ggdeck.</span>
           <DesktopMenu />
           <MobileMenu />
@@ -23,16 +23,15 @@ export default function AppLayout() {
         </div>
       </div>
       <div className="min-h-screen flex flex-col items-center">
-        <div className="bg-zinc-900 w-full p-8 flex flex-col items-center gap-8 justify-center sm:flex-row">
-          <div className="flex flex-col gap-6 items-center">
+        <div className="bg-zinc-900 w-full p-8 flex flex-col items-center gap-4 justify-center sm:flex-row">
+          <div className="flex flex-col gap-3 items-center">
             <Avatar className="w-28 h-28">
               <AvatarImage src="https://github.com/devalexsantos.png" />
               <AvatarFallback>AS</AvatarFallback>
             </Avatar>
             <span className="text-xl font-bold ">Alex Santos</span>
+            <ResumeProfile />
           </div>
-
-          <ResumeProfile />
         </div>
         <Outlet />
       </div>
