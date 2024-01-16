d8c1d67eee36f1636f2881fc7f2c1bc5a32cd633
Alex Santos <devalexsantos@gmail.com>
2024-01-16 14:50:56 -0300

    chore: change rout profile-dashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom'
-import Dashboard from './pages/app/dashboard'
+import Dashboard from './pages/app/profile-dashboard'
 import AppLayout from './pages/_layout/app'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [{ path: '/:userId', element: <Dashboard /> }],
   },
 ])
